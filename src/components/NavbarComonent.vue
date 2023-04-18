8f73355a3895aec091aa44f25dac87705e9340d7
mareesz1 <mike.marcell@students.jedlik.eu>
2023-04-18 09:59:32 +0200

    navbar fix

--- src/components/NavbarComonent.vue
+++ src/components/NavbarComonent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="nbar">
-    <div class="container mx-4">
+    <div class="mx-4">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-search mx-5" viewBox="0 0 16 16">
@@ -19,8 +19,8 @@
         </li>
        
       </ul>
-      <div class="nav-item sticky-right">  
-        <a href="/tanar"  class=" nav-link">Tanár vagy?</a>
+      <div class="nav-item" id="tanarvagy">  
+        <router-link to="/tanar" href="/tanar"  class=" nav-link">Tanár vagy?</router-link>
          </div>
     </div>
   </div>
@@ -40,6 +40,11 @@
     background-color: rgb(3, 187, 61);
     width: 100%;
     color: white;
+}
+
+#tanarvagy{
+  align-items: flex-end;
+  margin-right: 5em;
 }
 
 
