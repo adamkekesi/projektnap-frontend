2ac7c5edde313d7214d19adc4ee51e8e1a88c568
mareesz1 <mike.marcell@students.jedlik.eu>
2023-04-18 09:30:51 +0200

    routing alap

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 const app = createApp(App)
 
