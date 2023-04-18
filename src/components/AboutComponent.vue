7a8a726145b0bbf9e97b1dcf1dd2aea822d8c211
ngyGergo <nagy.gergo.roland@students.jedlik.eu>
2023-04-18 14:31:26 +0200

    GoalCompoent

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
-        <div class="text-center">
-            <h2 class="m-3 mb-4">Üdvözöllek a Tanárkereső oldalon!</h2>
+        <div class="text-center m-5">
+            <h2 class="mb-4">Üdvözöllek a Tanárkereső oldalon!</h2>
             <p>
                 Célunk az, hogy összekapcsoljunk minőségi oktatókat és tanulókat. A Tanárkereső egy olyan platform, amely
                 lehetővé teszi a tanulók számára, hogy könnyen és gyorsan megtalálják azokat a tanárokat, akik megfelelő
@@ -17,7 +17,7 @@
             <p>
                 Az oldalunkon keresztül könnyedén kereshetsz olyan tanárokat, akik megfelelnek az igényeidnek, legyen szó akár matek, nyelvtanulás vagy bármilyen más tantárgy oktatásáról. Minden tanár profilja részletes információkat tartalmaz a szaktudásáról, tapasztalatáról és az oktatási módszereiről. Emellett a tanulók véleményeit és értékeléseit is megtekintheted a tanárokról, így könnyedén megtalálhatod azokat az oktatókat, akik a leginkább megfelelnek az elvárásaidnak.
             </p>
-            <h6 class="mt-5">
+            <h6 class="mt-3">
                 Bízunk benne, hogy az oldalunkon megtalálod a megfelelő tanárt, aki segít neked a tanulásban és előrelépésben, valamint fejleszti a készségeidet és tudásodat. Ha bármilyen kérdésed van az oldalunkkal kapcsolatban, kérjük, ne habozz kapcsolatba lépni velünk. Köszönjük, hogy minket választottál!
             </h6>
         </div>
@@ -31,5 +31,8 @@
 <style lang="css" scoped>
 p {
     text-align: justify;
+}
+h2{
+    text-transform: uppercase;
 }
 </style>
