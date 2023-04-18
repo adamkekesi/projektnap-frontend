2f84b970d5bb57cc66fa081304d92ec5ef9d32a4
laladornyei <laladornyei@gmail.com>
2023-04-18 12:10:50 +0200

    search

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,33 +1,105 @@
 <template>
-    <div class="row">
-
-        <div class="d-flex justify-content-center m-4">
-           
-            <div class="input-group">
-                <input type="search" id="form1" class="form-control" />
-                <button type="button" class="btn btn-success">
-                    Keresés
-                </button>
+    <div class="container">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-6 align-content-center m-4">
+                <div class="input-group m-4">
+                    <input type="search" id="form1" class="form-control" placeholder="Keresés..." />
+                    <button type="button" class="btn btn-success btn-lg">
+                        Keresés
+                    </button>
+                </div>
             </div>
+            <div class="col"></div>
         </div>
     </div>
+    <div class="container">
 
-    <div class="btn-group">
-  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Action
-  </button>
-  <div class="dropdown-menu">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="#">Separated link</a>
-  </div>
-</div>
+
+        <div class="row">
+            <div class="d-flex">
+                <h5 class="szures">Szűrés:</h5>
+
+                <div class="dropdown col">
+                    <button class="btn btn-success dropdown-toggle btn-lg" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="../assets/teacher.png" class="img-fluid" alt="tantargy" style="padding: 2px 10px;" />
+                        Tantárgy
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+
+                <div class="dropdown col">
+                    <button class="btn btn-success dropdown-toggle btn-lg" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="../assets/vector.png" class="img-fluid" alt="tantargy" style="padding: 2px 10px;" />
+                        Szint
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+
+                <div class="dropdown col">
+                    <button class="btn btn-success dropdown-toggle btn-lg" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="../assets/location.png" class="img-fluid" alt="tantargy" style="padding: 2px 10px;" />
+                        Vármegye
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+
+                <div class="dropdown col">
+                    <button class="btn btn-success dropdown-toggle btn-lg" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="../assets/star.png" class="img-fluid" alt="tantargy" style="padding: 2px 10px;" />
+                        Értékelés
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+
+                <div class="dropdown col">
+                    <button class="btn btn-success dropdown-toggle btn-lg" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="../assets/coin.png" class="img-fluid" alt="tantargy" style="padding: 2px 10px;" />
+                        Árazás
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.dropdown-toggle {
+    width: 200px !important;
+}
+.szures{
+    margin-right: 30px;
+    margin-top: 10px;
+}
+</style>
