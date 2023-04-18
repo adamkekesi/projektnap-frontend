a312ced953feff22b8ab91170783c9ca5ed64696
SulyokDavid <113507775+SulyokDavid@users.noreply.github.com>
2023-04-18 13:51:15 +0200

    cards for teachers

--- src/components/SelectedTeacherCardComponent.vue
+++ src/components/SelectedTeacherCardComponent.vue
@@ -5,61 +5,46 @@
       <h1>Name</h1>
     </div>
     <div class="card-body px-4">
-      <img class="card-img-top" src="..." alt="Tanár profilkép" />
-      <hr/>
+      <img class="teacherImg" src="https://media.istockphoto.com/id/1160927409/photo/portrait-of-female-elementary-school-teacher-standing-in-classroom.jpg?s=612x612&w=0&k=20&c=cShdgor3DDR2suDcbD0cTIj4k-rAzHKztxnWU4nvTbs=" alt="...">
+      <h4>Tanár neve</h4>
 
-      <p class="mx-3">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
-          <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
-          <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+      <div class="body">
+        <div class="bodyItem">
+        <svg class="cardSvg" width="30" height="30" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.75 4.21673L6.7167 10.7667C3.50003 12.8667 3.50003 17.5667 6.7167 19.6667L16.75 26.2167C18.55 27.4 21.5167 27.4 23.3167 26.2167L33.3 19.6667C36.5 17.5667 36.5 12.8834 33.3 10.7834L23.3167 4.23339C21.5167 3.03339 18.55 3.03339 16.75 4.21673Z" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M9.38337 21.8L9.3667 29.6167C9.3667 31.7333 11 34 13 34.6667L18.3167 36.4333C19.2334 36.7333 20.75 36.7333 21.6834 36.4333L27 34.6667C29 34 30.6334 31.7333 30.6334 29.6167V21.8833" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M35.6667 25V15" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
-      </p>
+        <span class="badge math">Matek</span>
+      </div>
 
-      <p class="mx-3">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-text" viewBox="0 0 16 16">
-          <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-          <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
-          <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
-        </svg>
-      </p>
+      <div class="bodyItem">
+                <svg class="cardSvg" width="30" height="30" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 22.3831C22.8719 22.3831 25.2 20.0551 25.2 17.1831C25.2 14.3113 22.8719 11.9832 20 11.9832C17.1282 11.9832 14.8 14.3113 14.8 17.1831C14.8 20.0551 17.1282 22.3831 20 22.3831Z" stroke="white" stroke-width="3"/>
+                    <path d="M6.03326 14.15C9.31659 -0.28335 30.6999 -0.266681 33.9666 14.1666C35.8832 22.6333 30.6166 29.8 25.9999 34.2333C22.6499 37.4666 17.3499 37.4666 13.9833 34.2333C9.38326 29.8 4.11659 22.6166 6.03326 14.15Z" stroke="white" stroke-width="3"/>
+                </svg>
+                <span class="cardText location" >Lakhely</span>
+        </div>
+      </div>
 
-      <hr/>
+      <div class="info">
+        <h5>Elérhetőségek</h5>
+        <div class="infoItem">
+          <svg class="infoSvg" width="30" height="30" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M35.4166 42.7084H14.5833C8.33329 42.7084 4.16663 39.5834 4.16663 32.2917V17.7084C4.16663 10.4167 8.33329 7.29175 14.5833 7.29175H35.4166C41.6666 7.29175 45.8333 10.4167 45.8333 17.7084V32.2917C45.8333 39.5834 41.6666 42.7084 35.4166 42.7084Z" stroke="#F9FBFC" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M35.4167 18.75L28.8959 23.9583C26.75 25.6667 23.2292 25.6667 21.0834 23.9583L14.5834 18.75" stroke="#F9FBFC" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          <span>asd@gmail.com</span>
+        </div>
 
-      <h5 class="card-title">Elérhetőségek</h5>
-      <p class="card-text">tanár adatai</p>
-      <p class="mx-3">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-telephone"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
-          />
-        </svg>
-      </p>
-        
-      <p class="mx-3">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-envelope"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"
-          />
-        </svg>
-      </p>
+        <div class="infoItem">
+          <svg width="30" height="30" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M45.7708 38.1876C45.7708 38.9376 45.6041 39.7084 45.25 40.4584C44.8958 41.2084 44.4375 41.9167 43.8333 42.5834C42.8125 43.7084 41.6875 44.5209 40.4166 45.0417C39.1666 45.5626 37.8125 45.8334 36.3541 45.8334C34.2291 45.8334 31.9583 45.3334 29.5625 44.3126C27.1666 43.2917 24.7708 41.9167 22.3958 40.1876C20 38.4376 17.7291 36.5001 15.5625 34.3542C13.4166 32.1876 11.4791 29.9167 9.74996 27.5417C8.04163 25.1667 6.66663 22.7917 5.66663 20.4376C4.66663 18.0626 4.16663 15.7917 4.16663 13.6251C4.16663 12.2084 4.41663 10.8542 4.91663 9.60425C5.41663 8.33341 6.20829 7.16675 7.31246 6.12508C8.64579 4.81258 10.1041 4.16675 11.6458 4.16675C12.2291 4.16675 12.8125 4.29175 13.3333 4.54175C13.875 4.79175 14.3541 5.16675 14.7291 5.70841L19.5625 12.5209C19.9375 13.0417 20.2083 13.5209 20.3958 13.9792C20.5833 14.4167 20.6875 14.8542 20.6875 15.2501C20.6875 15.7501 20.5416 16.2501 20.25 16.7292C19.9791 17.2084 19.5833 17.7084 19.0833 18.2084L17.5 19.8542C17.2708 20.0834 17.1666 20.3542 17.1666 20.6876C17.1666 20.8542 17.1875 21.0001 17.2291 21.1667C17.2916 21.3334 17.3541 21.4584 17.3958 21.5834C17.7708 22.2709 18.4166 23.1667 19.3333 24.2501C20.2708 25.3334 21.2708 26.4376 22.3541 27.5417C23.4791 28.6459 24.5625 29.6667 25.6666 30.6042C26.75 31.5209 27.6458 32.1459 28.3541 32.5209C28.4583 32.5626 28.5833 32.6251 28.7291 32.6876C28.8958 32.7501 29.0625 32.7709 29.25 32.7709C29.6041 32.7709 29.875 32.6459 30.1041 32.4167L31.6875 30.8542C32.2083 30.3334 32.7083 29.9376 33.1875 29.6876C33.6666 29.3959 34.1458 29.2501 34.6666 29.2501C35.0625 29.2501 35.4791 29.3334 35.9375 29.5209C36.3958 29.7084 36.875 29.9792 37.3958 30.3334L44.2916 35.2292C44.8333 35.6042 45.2083 36.0417 45.4375 36.5626C45.6458 37.0834 45.7708 37.6042 45.7708 38.1876Z" stroke="white" stroke-width="3" stroke-miterlimit="10"/>
+          </svg>
+          <span>+36 30 ......</span>
+        </div>
+      </div>
 
-      <p>
-        
-      </p>
     </div>
   </div>
 </template>
@@ -70,19 +55,55 @@ const props = defineProps(['teacher']);
 
 <style lang="scss" scoped>
 .card-body {
-  background-color: rgba(0, 200, 0, 0.5);
-  border-bottom-right-radius: 1em !important;
-  border-bottom-left-radius: 1em !important;
-}
+  background-color: #00813B;
+  // border-bottom-right-radius: 1em !important;
+  // border-bottom-left-radius: 1em !important;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 
-.card-header {
-  border-top-right-radius: 1em !important;
-  border-top-left-radius: 1em !important;
-  background-color: rgb(3, 187, 61);
+  border-radius: 20px;
 }
 
 .card {
-  --bs-card-border-width: 0px !important;
-  height: 40rem;
+  border-radius: 20px;
+  height: 80vh;
+  color: white;
+
+}
+
+.teacherImg{
+    width: 180px;
+    height: 180px;
+    border-radius: 300px;
+    object-fit: cover;
+}
+
+h4{
+  margin: 20px 0;
+}
+
+.math{
+    background-color: #0017EA;
+    margin: 0 5px;
+}
+
+.bodyItem{
+  margin-bottom: 10px;
+}
+
+.info{
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.infoSvg{
+  margin-right: 10px;
+}
+
+.infoItem{
+  margin: 10px 0;
 }
 </style>
--- src/components/SelectedTeacherInfoCardComponent.vue
+++ src/components/SelectedTeacherInfoCardComponent.vue
@@ -1,9 +1,48 @@
 <template>
-  <div class="card" style="width: 50rem">
-  <div class="card-header text-center">
-    <h1>Foglalás</h1>
-  </div>
-    <div class="card-body">Teacher Information</div>
+  <div class="card">
+    <div class="card-body">
+        <h4>Óra foglalás</h4>
+        <div class="tableContainer">
+          <table>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </table>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -11,18 +50,39 @@
 
 <style lang="scss" scoped>
     .card-body {
-    background-color: rgba(0, 200, 0, 0.5);
+    background-color: #00813B;
     border-bottom-right-radius: 1em !important;
     border-bottom-left-radius: 1em !important;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
   }
   
   .card-header {
     border-top-right-radius: 1em !important;
     border-top-left-radius: 1em !important;
-    background-color: rgb(3, 187, 61);
+    background-color: #00813B;
+
   }
   .card {
     --bs-card-border-width: 0px !important;
-    height: 40rem;
+    height: 80vh;
+    width: 60vw;
+    background-color: #00813B;
+    border-radius: 20px;
+  }
+
+  h4{
+    color: white;
+  }
+
+  table, td, tr{
+    border: 1px solid white;
+  }
+
+  table{
+    width: 50vw;
+    height: calc(80vh - 130px);
   }
 </style>
